23a15a7f755e0cdcb06181728c7b3819fbab13c0
David Ahn <davidahn1108@gmail.com>
2025-12-20 23:16:45 -0500

    modified the websiteview.js. This is fetch the data from backend once the backend works on a web server

--- frontEnd/websiteView.js
+++ frontEnd/websiteView.js
@@ -5,6 +5,21 @@ const answer = document.getElementById("answer");
 
 
 snowButton.addEventListener("click", () => {
-  answer.textContent = num1.value + " + " + num2.value + " = "  + (Number(num1.value) + Number(num2.value));
+  const city = num1.value;
+  const state = num2.value;
+
+  fetch(`/get_forecast?city=${encodeURIComponent(city)}&state=${encodeURIComponent(state)}`) 
+    .then(response => response.json()) 
+    .then (data => {
+      if(data.forecast) {
+        answer.textContent = data.forecast;
+      }
+      else {
+        answer.textContent = "Error: " + data.error;
+      }
+    })
+  .catch(err=> {
+    answer.textContent = "Fetch Data Request failed: " + err;
+  })
 })
 
