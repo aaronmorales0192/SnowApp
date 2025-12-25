f08ed059d4945db874db33bbd6763fa84ed5e859
Aaron <aaronmsomorales@gmail.com>
2025-12-25 10:22:51 -0500

    flask edit

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,6 +1,15 @@
 import data
 import snowLogic
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 #print(snowLogic.__file__)
 print("enter a city:")
