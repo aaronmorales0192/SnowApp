da57127570881c6fe14a328e643f9f198624eaa2
David Ahn <davidahn1108@gmail.com>
2025-12-26 22:35:29 -0500

    added current local time logic. please test

--- backEnd/snowLogic.py
+++ backEnd/snowLogic.py
@@ -2,43 +2,37 @@
 import requests
 from datetime import datetime
 
-#parts for improvement: Eventually the API we are using does not update every hour, updates at a sepcific interval. 
-#this means that the current logic implemented will only get the precipatation amount of the first time given even though 
-#the current time has passed. Also, the temp_2m is also wrong in logic. 
-#need to implement a method that automatically updates the value we are getting based on current time.
 def get_forecast_summary(lat, lon):
+    #so the logic has been fixed so it works based on the user's local time. However, I was not able to test, since flask server 
+    #doesn't work for now. Also, if you want entire day worth of data, tell me.
+
     #"""Fetch forecast data and return a readable string summary."""
     url = "https://api.open-meteo.com/v1/forecast?latitude=" + str(lat) + "&longitude=" + str(lon) + "&hourly=temperature_2m,precipitation,precipitation_type&forecast_days=2&timezone=America%2FNew_York"
     r = requests.get(url, timeout=10) #possible url change needed if time passes. Otherwise, it looks good
     r.raise_for_status()
     data = r.json() #i believe this converts the json into a dictionary, not string
     temps = data["hourly"]["temperature_2m"]
-    precip = data["hourly"]["precipitation"]
-    types = data["hourly"]["precipitation_type"] #null in data set for some reason
     current_time = datetime.now() #gives us the current local time of the user.
 
+    current_hour = current_time.hour 
     # Compute simple stats
-    avg_temp = sum(temps) / len(temps) 
-    #need to modify this logic. eventually the temp_2m doesnt mean temperature in 2 min interval, 
-    #it means that the temperature is recorded at 2m above the ground.
 
-    precipAndType = zip(types, precip)
+    #this part takes care of the edge case: in case the API decreases time to 24 hours, this will prevent from getting index error.
+    next_hour = min(current_hour + 1, len(temps) - 1)
+    next_hour_temp = temps[next_hour] 
+    #This logic will get the current local time and get the temperature of next hour from current.
+
     snow_hours = 0
-    # zip pairs each precip type with corresponding precip amount
-    for type, precip in precipAndType:
-        if type == 2 and precip > 0:
-            snow_2Hours+=1
-
     #since the return type of function is a tuple, need to initialize two variables at once.
-    snow_chance, snow_amount = get_snowfall_chance_amount_ensemble(lat, lon)
+    snow_chance, snow_amount = get_snowfall_chance_amount_ensemble(lat, lon, next_hour)
 
     # Return as a formatted string
-    summary = f"❄️ Snow chance: {snow_chance:.1f}% | 🌨️ Snow amount: {snow_amount:.2f} inches | 🌡️ Avg Temp: {avg_temp:.1f}°C"
+    summary = f"❄️ Snow chance: {snow_chance:.1f}% | 🌨️ Snow amount: {snow_amount:.2f}cm | 🌡️ Temperature: {next_hour_temp:.1f}°C"
     return summary
 
 #more accurate for snowfall prediction as it accounts for snowfall amounts by ensemble member
 #utilizes api from gfs seamless ensembles
-def get_snowfall_chance_amount_ensemble(lat, lon):
+def get_snowfall_chance_amount_ensemble(lat, lon, next_hour):
     GFS_url = "https://ensemble-api.open-meteo.com/v1/ensemble?latitude=" +  str(lat) + "&longitude="  + str(lon) + "&hourly=snowfall&models=gfs_seamless&forecast_days=7&timezone=America/New_York"
     
     r = requests.get(GFS_url, timeout=10) #possible url change needed if time passes. Otherwise, it looks good
@@ -53,9 +47,9 @@ def get_snowfall_chance_amount_ensemble(lat, lon):
     for i in range(1, 31):
         currentMember = f"snowfall_member{i:02d}"
         #for now, the loop only predicts for the next hour. If we want more hours like 24, then I can modify the logic to do so.
-        if (GFS_MODEL_data["hourly"][currentMember][0] != 0):
+        if (GFS_MODEL_data["hourly"][currentMember][next_hour] != 0):
             nextHourSnowCount+=1
-            nextHourSnowAmount+=GFS_MODEL_data["hourly"][currentMember][0]
+            nextHourSnowAmount+=GFS_MODEL_data["hourly"][currentMember][next_hour]
     
     #get the average percent of snowing chance
     snowPercent = (nextHourSnowCount/30)*100
