3369ead752767e1ed83046d8536f37c3b07e72bf
Aaron <aaronmsomorales@gmail.com>
2025-12-26 22:56:41 -0500

    added interactive loop for testing file

--- backEnd/test.py
+++ backEnd/test.py
@@ -3,9 +3,13 @@ import data
 import snowLogic
 
 #print(snowLogic.__file__)
-print("enter a city:")
-city = input()
-print("enter a state:")
-state = input()
-lat, lon = data.get_coordinates(city, state)
-print(snowLogic.get_forecast_summary(lat, lon))
+city = None; 
+while True:
+    print("enter a city: (enter \"exit\" to exit)")
+    city = input()
+    if city == "exit":
+        break
+    print("enter a state:")
+    state = input()
+    lat, lon = data.get_coordinates(city, state)
+    print(snowLogic.get_forecast_summary(lat, lon))
